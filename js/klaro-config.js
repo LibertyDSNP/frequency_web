abb03b1d681a9fb628573c6d26a7cdc7653dd833
industriousbird <jk@industriousbird.com>
2022-06-28 19:51:47 -0700

    edited klaro styles, added cookie page

--- js/klaro-config.js
+++ js/klaro-config.js
@@ -63,6 +63,8 @@ var klaroConfig = {
             },
             acceptAll:
                 'Accept All',
+            acceptSelected:
+                'Accept Selected',    
             matomo: {
                 description:
                     "Privacy focused site analytics with no cross site tracking",
